a78022537d58639eb9fa029b9b346804bd92ac35
Wilson Gantt <wilsongantt@yahoo.com>
2026-02-16 19:48:17 -0500

    Re-adding hibernate JPA auto config exclusion

--- src/main/java/we/employ/you/WeEmployYouApplication.java
+++ src/main/java/we/employ/you/WeEmployYouApplication.java
@@ -2,8 +2,9 @@ package we.employ.you;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class WeEmployYouApplication {
 
 	public static void main(String[] args) {
