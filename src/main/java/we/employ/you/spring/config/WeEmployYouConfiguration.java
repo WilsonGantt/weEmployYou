e9865880a3e6c5934e2d7167b8f0f2d26ceb8c55
Wilson Gantt <wilsongantt@yahoo.com>
2026-02-23 18:37:40 -0500

    Adding EntityManagerFactory bean

--- src/main/java/we/employ/you/spring/config/WeEmployYouConfiguration.java
+++ src/main/java/we/employ/you/spring/config/WeEmployYouConfiguration.java
@@ -7,6 +7,8 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,7 +18,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableTransactionManagement
 public class WeEmployYouConfiguration {
 
-    private final Properties hibernateProperties() {
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+
+		emf.setDataSource(dataSource());
+		emf.setPackagesToScan("we.employ.you");
+
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setShowSql(true); 
+		emf.setJpaVendorAdapter(vendorAdapter);
+
+		emf.setJpaProperties(hibernateProperties());
+
+		return emf;
+	}
+
+    private Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
         hibernateProperties.setProperty(
           "hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
